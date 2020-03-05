2575f7ccfc45896cfd825dc98b98f48346bb7557
FreeWind94 <shukin-nikita@mail.ru>
2020-03-05 15:40:52 +0300

    Add project files.

---
+++ PicRotate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PicRotate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path;
+                if (args.Length == 1)
+                {
+                    path = args[0];
+                }
+                else
+                {
+                    throw new ArgumentException("Ошибка: Программа должна быть запушена с одним параметром: путь к диррентории в которой она должна работать.");
+                }
+
+                if (!Directory.Exists(path))
+                {
+                    Console.WriteLine("Директории: " + path + " не существует.");
+                }
+
+
+                Console.WriteLine("Запуск программы в директории: " + path);
+                string[] files = Directory.GetFiles(path, "*.bmp");
+                Console.WriteLine("Найдено " + files.Length + " файлов формата bmp");
+                Parallel.ForEach(files, RotateTools.Rotate);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.Write("Завершено! Для выхода нажмите любую клавишу...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PicRotate/RotateTools.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace PicRotate
+{
+    class RotateTools
+    {
+        static public void Rotate(string imagePath)
+        {
+            try
+            {
+                Bitmap image = new Bitmap(imagePath, true);
+                image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                Console.WriteLine("Поворот изображения: " + imagePath);
+                string imageNewPath = imagePath.Replace(".bmp", "_rotaded.bmp");
+                image.Save(imageNewPath, System.Drawing.Imaging.ImageFormat.Bmp);
+                Console.WriteLine("Изображение: " + imagePath + " успешно повёрнуто и сохранено как: " + imageNewPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
